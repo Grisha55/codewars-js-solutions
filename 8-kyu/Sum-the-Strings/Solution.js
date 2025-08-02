543b0a1ca4adbb7eb94f27ec271f7b14f0d02b0c
Grisha55 <grishavinyar@icloud.com>
2025-08-03 00:16:24 +0300

    Added Sum-The-Strings solution

---
+++ 8-kyu/Sum-the-Strings/Solution.js
@@ -0,0 +1,11 @@
+/**
+ * @function sumStr
+ * @param {string} a
+ * @param {string} b
+ * @returns {string}
+*/
+
+function sumStr(a,b) {
+  const sum = Number(a) + Number(b);
+  return String(sum);
+}
