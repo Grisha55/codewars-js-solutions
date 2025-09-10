59fd4af1e1fa06bebd07f998f3c51de6f0817bf1
Grisha55 <grishavinyar@icloud.com>
2025-09-11 01:19:06 +0300

    Added Solution.js

---
+++ 8-kyu/Simple-multiplication/Solution.js
@@ -0,0 +1,9 @@
+/**
+ * @function simpleMultiplication
+ * @param {number} number
+ * @returns {number}
+ */
+
+function simpleMultiplication(number) {
+  return number % 2 === 0 ? number * 8 : number * 9;
+}
