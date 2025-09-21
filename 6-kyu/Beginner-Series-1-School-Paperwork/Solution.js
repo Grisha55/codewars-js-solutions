15d6e3e6164badaa60946354f219728901fdbe26
Grisha55 <grishavinyar@icloud.com>
2025-09-21 10:09:11 +0300

    Added Solution.js

---
+++ 6-kyu/Beginner-Series-1-School-Paperwork/Solution.js
@@ -0,0 +1,12 @@
+/**
+ * @function paperwork
+ * @param {number} n
+ * @param {number} m
+ * @returns {number}
+ */
+
+function paperwork(n, m) {
+  if (n < 0 || m < 0) return 0;
+
+  return n * m;
+}
