9458e142ba94030d269db602402fc0fff7fa7783
Grisha55 <grishavinyar@icloud.com>
2025-09-30 11:10:22 +0300

    Added Solution.js

---
+++ 8-kyu/Volume-of-a-Cuboid/Solution.js
@@ -0,0 +1,14 @@
+/**
+ * @function getVolumeOfCuboid
+ * @param {number} length
+ * @param {number} width
+ * @param {number} height
+ * @returns {number}
+ */
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    const volume = length * width * height;
+    return volume;
+  }
+}
