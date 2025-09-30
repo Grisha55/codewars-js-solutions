36c17d7e6caf04fb781bb0e65f7e93a170648486
Grisha55 <grishavinyar@icloud.com>
2025-09-30 11:14:25 +0300

    Added Solution.js

---
+++ 8-kyu/Is-he-gonna-survive?/Solution.js
@@ -0,0 +1,10 @@
+/**
+ * @function hero
+ * @param {number} bullets
+ * @param {number} dragons
+ * @returns {boolean}
+ */
+
+function hero(bullets, dragons) {
+  return bullets / 2 >= dragons ? true : false;
+}
