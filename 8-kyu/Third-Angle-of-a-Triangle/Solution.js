f5c6b6c1cc2c6bc77ba02b4624b3e2d97e18baa2
Grisha55 <grishavinyar@icloud.com>
2025-10-01 10:17:47 +0300

    Added Solution.js

---
+++ 8-kyu/Third-Angle-of-a-Triangle/Solution.js
@@ -0,0 +1,11 @@
+/**
+ * @function otherAngle
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+
+function otherAngle(a, b) {
+  const angle = 180 - (a + b);
+  return angle;
+}
