726b8fe64623710258dd4d478405f7ce86b8c7b5
Grisha55 <grishavinyar@icloud.com>
2025-10-06 11:43:23 +0300

    Added Solution.js

---
+++ 8-kyu/Welcome/Solution.js
@@ -0,0 +1,33 @@
+/**
+ * @function greet
+ * @param {string} language
+ * @returns {string}
+ */
+
+function greet(language) {
+  const languages = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso',
+  };
+
+  if (languages[language]) {
+    return languages[language];
+  } else {
+    return 'Welcome';
+  }
+}
