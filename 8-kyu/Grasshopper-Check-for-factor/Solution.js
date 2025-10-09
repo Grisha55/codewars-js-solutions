ec7eb06a3a76396f9ece3da4359f625a8955e5d2
Grisha55 <grishavinyar@icloud.com>
2025-10-09 20:24:20 +0300

    Added Solution.js

---
+++ 8-kyu/Grasshopper-Check-for-factor/Solution.js
@@ -0,0 +1,10 @@
+/**
+ * @function checkForFactor
+ * @param {number} base
+ * @param {number} factor
+ * @returns {boolean}
+ */
+
+function checkForFactor(base, factor) {
+  return base % factor == 0;
+}
