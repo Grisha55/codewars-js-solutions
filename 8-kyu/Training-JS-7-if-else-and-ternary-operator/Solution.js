6f66e5526594b1410f31e9ad2759923b434a6d02
Grisha55 <grishavinyar@icloud.com>
2025-10-15 10:52:48 +0300

    Added Solution.js

---
+++ 8-kyu/Training-JS-7-if-else-and-ternary-operator/Solution.js
@@ -0,0 +1,9 @@
+/**
+ * @function saleHotdogs
+ * @param {number} n
+ * @returns {number}
+ */
+
+function saleHotdogs(n) {
+  return n < 5 ? n * 100 : n >= 5 && n < 10 ? n * 95 : n >= 10 ? n * 90 : 0;
+}
