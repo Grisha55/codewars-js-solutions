bb910a1b3b36f55bc52212133d6ae026ef757187
Grisha55 <grishavinyar@icloud.com>
2025-10-26 13:20:13 +0300

    Added Solution.js

---
+++ 8-kyu/Find-the-first-non-consecutive-number/Solution.js
@@ -0,0 +1,15 @@
+/**
+ * @function firstNonConsecutive
+ * @param {number[]} arr
+ * @returns {number}
+ */
+
+function firstNonConsecutive(arr) {
+  if (arr.length < 2) return null;
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] > 1) return arr[i];
+  }
+
+  return null;
+}
