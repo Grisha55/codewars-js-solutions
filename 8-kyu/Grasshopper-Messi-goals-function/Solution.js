266d2823567565afcae5bc8e1503a3ba14c05899
Grisha55 <grishavinyar@icloud.com>
2025-10-27 11:48:48 +0300

    Added Solution.js

---
+++ 8-kyu/Grasshopper-Messi-goals-function/Solution.js
@@ -0,0 +1,11 @@
+/**
+ * @function goals
+ * @param {number} laLigaGoals
+ * @param {number} copaDelReyGoals
+ * @param {number} championsLeagueGoals
+ * @returns {number}
+ */
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
