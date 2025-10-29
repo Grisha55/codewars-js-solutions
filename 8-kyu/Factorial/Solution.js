9f339328c31f1b8a37c0d123d02be14b5d1156c5
Grisha55 <grishavinyar@icloud.com>
2025-10-29 13:54:09 +0300

    Added Solution.js

---
+++ 8-kyu/Factorial/Solution.js
@@ -0,0 +1,14 @@
+/**
+ * @function factorial
+ * @param {number} n
+ * @return {number}
+ */
+
+function factorial(n) {
+  if (n === 0) return 1;
+  if (n < 0 || n > 12) {
+    throw RangeError();
+  }
+
+  return n * factorial(n - 1);
+}
