73db777320cf1a89a8f52500f34211119e09a1d8
Grisha55 <grishavinyar@icloud.com>
2025-11-01 13:33:35 +0300

    Added Solution.js

---
+++ 8-kyu/How-many-lightsabers-do-you-own/Solution.js
@@ -0,0 +1,9 @@
+/**
+ * @function howManyLightsabersDoYouOwn
+ * @param {string} name
+ * @returns {number}
+ */
+
+function howManyLightsabersDoYouOwn(name) {
+  return name === 'Zach' ? 18 : 0;
+}
