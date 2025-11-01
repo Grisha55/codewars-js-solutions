e2792d43790c999a1724625d84ff072c03a3b189
Grisha55 <grishavinyar@icloud.com>
2025-11-01 13:30:25 +0300

    Added Solution.js

---
+++ 8-kyu/The-Wide-Mouthed-frog/Solution.js
@@ -0,0 +1,9 @@
+/**
+ * @function mouthSize
+ * @param {string} animal
+ * @returns {string}
+ */
+
+function mouthSize(animal) {
+  return animal.toLowerCase() === 'alligator' ? 'small' : 'wide';
+}
