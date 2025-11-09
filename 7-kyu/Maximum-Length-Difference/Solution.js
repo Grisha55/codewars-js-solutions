a92d5b16360c47e30895bed023961da4ab977a6a
Grisha55 <grishavinyar@icloud.com>
2025-11-09 12:33:57 +0300

    Added Solution.js

---
+++ 7-kyu/Maximum-Length-Difference/Solution.js
@@ -0,0 +1,18 @@
+/**
+ * @function mxdiflg
+ * @param {string[]} a1
+ * @param {string[]} a2
+ * @returns {number}
+ */
+
+function mxdiflg(a1, a2) {
+  if (a1.length === 0 || a2.length === 0) return -1;
+  let max = 0;
+  for (let i = 0; i < a1.length; i++) {
+    for (let j = 0; j < a2.length; j++) {
+      max = Math.max(max, Math.abs(a1[i].length - a2[j].length));
+    }
+  }
+
+  return max;
+}
