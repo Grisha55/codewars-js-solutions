f424e4e03de866e1563787511851e3bf696c2702
Grisha55 <grishavinyar@icloud.com>
2025-11-10 12:19:39 +0300

    Added Solution.js

---
+++ 8-kyu/Reversed-sequence/Solution.js
@@ -0,0 +1,13 @@
+/**
+ * @function reverseSeq
+ * @param {number} n
+ * @returns {number[]}
+ */
+
+const reverseSeq = (n) => {
+  let res = [];
+  for (let i = n; i > 0; i--) {
+    res.push(i);
+  }
+  return res;
+};
