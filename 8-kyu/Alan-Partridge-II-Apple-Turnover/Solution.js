8c0f55b7f0bfac0d1a47be53fd693bd499129ea5
Grisha55 <grishavinyar@icloud.com>
2025-12-05 12:59:36 +0300

    Added Solution.js

---
+++ 8-kyu/Alan-Partridge-II-Apple-Turnover/Solution.js
@@ -0,0 +1,11 @@
+/**
+ * @function apple
+ * @param {number || string} x
+ * @returns {string}
+ */
+
+function apple(x) {
+  return Number(x) * Number(x) > 1000
+    ? `It's hotter than the sun!!`
+    : 'Help yourself to a honeycomb Yorkie for the glovebox.';
+}
