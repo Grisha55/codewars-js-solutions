0417f3fef2d261a3e4a91a1a5ce546ca0424eb3d
Grisha55 <grishavinyar@icloud.com>
2025-12-06 10:59:57 +0300

    Added Solution.js

---
+++ 7-kyu/esreveR/Solution.js
@@ -0,0 +1,8 @@
+reverse = function (array) {
+  let res = [];
+
+  for (let i = array.length - 1; i >= 0; i--) {
+    res.push(array[i]);
+  }
+  return res;
+};
---
+++ 8-kyu/Plural/Solution.js
@@ -0,0 +1,9 @@
+/**
+ * @function plural
+ * @param {number} n
+ * @returns {boolean}
+ */
+
+function plural(n) {
+  return n !== 1;
+}
