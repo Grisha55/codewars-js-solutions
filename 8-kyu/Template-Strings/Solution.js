451ccc9e3b0bf594a747137b8655f15bfeb4357b
Grisha55 <grishavinyar@icloud.com>
2025-12-07 12:39:49 +0300

    Added Solution.js

---
+++ 8-kyu/Template-Strings/Solution.js
@@ -0,0 +1,10 @@
+/**
+ * @function templateStrings
+ * @param {string} noun
+ * @param {string} adjective
+ * @returns {string}
+ */
+
+var templateStrings = function (noun, adjective) {
+  return noun + ' are ' + adjective;
+};
