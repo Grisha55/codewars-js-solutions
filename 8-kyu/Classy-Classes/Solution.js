7247535c2b1bcb54f27c27069d8335e2ad0a9871
Grisha55 <grishavinyar@icloud.com>
2025-12-08 12:58:04 +0300

    Added Solution.js

---
+++ 8-kyu/Classy-Classes/Solution.js
@@ -0,0 +1,14 @@
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  get info() {
+    return this.name + 's' + ' age is ' + this.age;
+  }
+
+  getInfo() {
+    return this.info;
+  }
+}
