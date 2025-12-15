a8be0b03075741ec2eef5fc830001c38f8260dd9
Grisha55 <grishavinyar@icloud.com>
2025-12-15 10:43:48 +0300

    Added Solution.js

---
+++ 8-kyu/Multiplication-table-for-number/Solution.js
@@ -0,0 +1,18 @@
+/**
+ * @function multipleTable
+ * @param {number} number
+ * @returns {string}
+ */
+
+function multiTable(number) {
+  let res = '';
+
+  for (let i = 1; i <= 10; i++) {
+    res += `${i} * ${number} = ${i * number}`;
+    if (i < 10) {
+      res += '\n';
+    }
+  }
+
+  return res;
+}
