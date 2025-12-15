65d76bb0ae7624ff5efa39f1216a4904b1a43f9f
Grisha55 <grishavinyar@icloud.com>
2025-12-15 10:43:43 +0300

    Added Solution.js

---
+++ 8-kyu/ES6-string-addition/Solution.js
@@ -0,0 +1,10 @@
+/**
+ * @function joinStrings
+ * @param {string} string1
+ * @param {string} string2
+ * @return {string}
+ */
+
+function joinStrings(string1, string2) {
+  return `${string1} ${string2}`;
+}
