eee8fc72cabee7c0ab1fc70739558550d4fd7098
Grisha55 <grishavinyar@icloud.com>
2025-12-18 12:21:17 +0300

    Added Solution.js

---
+++ 7-kyu/Over-The-Road/Solution.js
@@ -0,0 +1,10 @@
+/**
+ * @function overTheRoad
+ * @param {number} address 
+ * @param {number} n 
+ * @returns {number}
+ */
+
+function overTheRoad(address, n){
+  return (n * 2 + 1) - address;
+}
