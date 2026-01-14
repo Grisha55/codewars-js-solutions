b564eb71385b1c5a1b813d6a09c2cc81d4026a7b
Grisha55 <grishavinyar@icloud.com>
2026-01-14 12:38:30 +0300

    Added Solution.js

---
+++ 7-kyu/Clean-up-after-your-dog/Solution.js
@@ -0,0 +1,16 @@
+function crap(x, bags, cap){
+	let fullPlaces = cap * bags;
+
+	for (gardens of x) {
+		for (garden of gardens) {
+			if (garden === '@') {
+				fullPlaces--;
+			}
+			if (garden === 'D') {
+				return 'Dog!!'
+			}
+		}
+	}
+
+	return fullPlaces >= 0 ? 'Clean' : 'Cr@p';
+}
