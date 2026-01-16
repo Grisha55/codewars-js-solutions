03c8cb12148ceb7134d12ae0f4d0906f1446a9fd
Grisha55 <grishavinyar@icloud.com>
2026-01-17 01:17:06 +0300

    Added Solution.js

---
+++ 8-kyu/Grasshopper-Create-the-rooms/Solution.js
@@ -0,0 +1,17 @@
+var rooms = {
+	first: {
+		name: 'first',
+		description: 'This is the first room',
+		completed: false,
+	},
+	second: {
+		name: 'second',
+		description: 'This is the second room',
+		completed: false,
+	},
+	third: {
+		name: 'third',
+		description: 'This is the third room',
+		completed: false,
+	},
+}
