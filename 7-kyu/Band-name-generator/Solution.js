745ae29feb75330dfe4b9ec7c5360dfdec54493d
Grisha55 <grishavinyar@icloud.com>
2026-01-24 02:08:16 +0300

    Added Solution.js

---
+++ 7-kyu/Band-name-generator/Solution.js
@@ -0,0 +1,7 @@
+function bandNameGenerator(str) {
+	if (str[0].toLowerCase() === str[str.length - 1].toLowerCase()) {
+		return (str[0].toUpperCase() + str.slice(1) + str.slice(1)).trim();
+	}
+
+	return 'The ' + str[0].toUpperCase() + str.slice(1);
+}
