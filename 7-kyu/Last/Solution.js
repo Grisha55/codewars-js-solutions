13e47efd487bf128781b9d963641f05228f36516
Grisha55 <grishavinyar@icloud.com>
2026-02-26 01:35:40 +0300

    Added Solution.js

---
+++ 7-kyu/Last/Solution.js
@@ -0,0 +1,11 @@
+function last(...args) {
+  
+  if (args.length > 1) return args[args.length-1];
+  if (args.length === 1) {
+    const value = args[0];
+    if (typeof value === 'string' || Array.isArray(value)) {
+      return value[value.length - 1];
+    }
+  }
+  return args[0];
+}
