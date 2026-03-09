25cab103e47c66252e974af7475bfc7d2bdc9aad
Grisha55 <grishavinyar@icloud.com>
2026-03-10 00:35:18 +0300

    Added Solution.js

---
+++ 7-kyu/Caffeine-Script/Solution.js
@@ -0,0 +1,17 @@
+function caffeineBuzz(n) {
+	if (n % 3 === 0 && n % 4 === 0) {
+		if (n % 2 === 0) {
+			return 'CoffeeScript';
+		} else {
+			return 'Coffee';
+		}
+	} else if (n % 3 === 0) {
+		if (n % 2 === 0) {
+			return 'JavaScript';
+		} else {
+			return 'Java';
+		}
+	} else {
+		return 'mocha_missing!';
+	}
+}
