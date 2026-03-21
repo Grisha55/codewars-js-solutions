b9a6c865d36b141313b05eaab0e623e832cb91cb
Grisha55 <grishavinyar@icloud.com>
2026-03-22 00:41:51 +0300

    Added Solution.js

---
+++ 8-kyu/Training-JS-#31-methods-of-arrayObject-arrayObject-isArray()-indexOf()-and-toString()/Solution.js
@@ -0,0 +1,13 @@
+function blackAndWhite(arr) {
+	if (!Array.isArray(arr)) {
+		return "It's a fake array";
+	}
+
+	if (arr.indexOf(5) !== -1 && arr.indexOf(13) !== -1) {
+		return "It's a black array";
+	} else {
+		return "It's a white array";
+	}
+
+	return undefined;
+}
